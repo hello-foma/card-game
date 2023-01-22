f810bb9dae413f24e4bd57f03d4750b8f1d75015
Foma Kolosov <f.kolosov@yclients.tech>
2023-01-23 00:11:59 +0300

    chore(user): create user slice

--- src/saga.ts
+++ src/saga.ts
@@ -2,11 +2,12 @@ import { all } from 'redux-saga/effects';
 
 import { saga as boardsSaga } from '@modules/boards/slice';
 import {saga as welcomeSaga} from '@modules/welcome/slice';
-
+import { saga as userSaga } from '@modules/user/slice'
 export default function* () {
   yield all([
     ...welcomeSaga,
-     ...boardsSaga,
+    ...boardsSaga,
+    ...userSaga,
     ]
   );
 }
--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,7 @@
 import boardsReducer from '@modules/boards/slice';
+import userReducer from '@modules/user/slice';
 
 export default {
-  boardsState: boardsReducer('boardsState')
+  boardsState: boardsReducer('boardsState'),
+  userState: userReducer('userState'),
 };
